451ee2618bc08ec83a4590ef35134237ded2636a
Wildanmz27 <wildan.zyan27@gmail.com>
2024-10-15 15:40:58 +0700

    Delete hapus.php

--- hapus.php
+++
@@ -1,9 +0,0 @@
-<?php  
-  //koneksi ke database
-    $konek = mysqli_connect("localhost", "root", "", "databel");
-    $id = $_GET['id'];
-    //hapus data
-    mysqli_query($konek, "delete from jam where id='$id'");
-    //kembali ke halaman utama
-    echo "<script>location.replace('index.php');</script>";
-?>
